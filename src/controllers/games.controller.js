7c4fb71fb8f6b85aa4f28bb42c501aee7be0a618
larissa <larissaalves1102@gmail.com>
2023-05-10 00:16:26 -0300

    feat: route POST/games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,8 +1,16 @@
 import { db } from "../database/database.connection.js";
 
 export async function createGame(req, res) {
-    try {
+    const { name, image, stockTotal, pricePerDay } = req.body;
 
+    if (!name || stockTotal < 0 || pricePerDay < 0) return res.sendStatus(400);
+
+    try {
+        const game = await db.query(`SELECT * FROM games WHERE name=$1;`, [name]);
+        if (game.rowCount > 0) return res.sendStatus(409);
+
+        await db.query(`INSERT INTO games ("name", "image", "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+        res.sendStatus(201);
     }
     catch (err) {
         res.status(500).send(err.message);
